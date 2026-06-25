62f1a4dacee4b4a4c477f4222ca88a9b988d0994
Maaz Tariq <maazt254@gmail.com>
2026-06-25 11:55:38 +0530

    Add user authentication with JWT and bcrypt

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
 const connectDB = require("./config/db");
 
 dotenv.config();
@@ -13,11 +14,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
     res.send("Server Running");
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/api/profile", protect, (req, res) => {
+    res.json({
+        message: "Protected Route Accessed",
+        user: req.user
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
